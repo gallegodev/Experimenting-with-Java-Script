15ae9cb34ef2d63bcc64d0972bde2fc9d9d76400
gallegodev <gallegodev1991@gmail.com>
2022-12-06 15:39:29 +0000

    Add logical structure of JS script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,10 @@
-var numberOfCats = 3;
+var price = 200;
+var discount = 30
+;
+var inStock = true;
 
-while(numberOfCats > 0) {
-    document.write("Klaus se comporta!!!!!<br>");
+if(price <= 200 && discount < 15 && inStock == true) {
+    alert("Buy everything!!!!")
+} else if (price > 200 || discount > 15 || inStock == false) {
+    alert("Do not buy, it a horrible offer!!!!")
 }
-document.write("Bom menino!!!!")
