24d361924a9e4d49dcc07e5527f093a9c6447a54
gallegodev <gallegodev1991@gmail.com>
2022-12-08 21:31:36 +0000

    Update

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,16 @@
-function studyMore(x, y){
-    var letMeKnow =  x * y / 2;
-    return letMeKnow;    
-}
+var theDog = {
+    name: "Fido",
+    weight: 30,
+    age: 5,
+    activity: bark
+};
 
-var level = 5;
-var exp = 100;
-var gettingStronger = studyMore(level, exp);
-alert(gettingStronger);
-
-function textOfEncouragement(){
-    if (gettingStronger >= 250){
-        console.log("You are getting better");
+function dogBarking(){
+    if (weight > 30){
+        console.log("WOOF WOOF!");
     } else {
-        console.log("Try harder");
+        console.log("woof woof");
     }
     
-}
-var text = textOfEncouragement();
-console.log(text);
+    
+} 
